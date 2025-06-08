0f70ca37e705fc647ecd8b3a82e51e1633158f5d
hajin148 <59868495+hajin148@users.noreply.github.com>
2025-06-07 18:37:38 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -65,17 +65,6 @@ function App() {
 
   const filteredToolCount = filteredTools.length;
 
-  if (loading) {
-    return (
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div>
-          <p className="mt-4 text-gray-600">Loading...</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="min-h-screen bg-gray-50">
       <AuthModal
