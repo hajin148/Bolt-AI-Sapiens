d3b506c224b4d06083b136703ef860bce47fb188
hajin148 <59868495+hajin148@users.noreply.github.com>
2025-06-29 14:36:12 -0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
       style={{ backgroundColor: '#181818', borderTop: '1px solid #374151' }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full" style={{ marginLeft: '330px' }}>
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full" style={{ marginLeft: '100px' }}>
           {/* 왼쪽: 로고 + 문구 */}
           <div className="mb-6 md:mb-0 w-full md:w-auto">
             <div className="flex items-center gap-3 justify-start mb-2">
